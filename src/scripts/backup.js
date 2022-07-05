fcdacad6b15640485bc902e05121dc49c72bebd7
efindus <48939809+efindus@users.noreply.github.com>
2022-07-05 21:07:38 +0200

    add backup script
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

---
+++ src/scripts/backup.js
@@ -0,0 +1,42 @@
+const { writeFileSync } = require('fs');
+const db = require('../utils/database');
+
+const main = async () => {
+	await db.connect();
+
+	const dbBackup = {
+		sessions: {},
+		messages: [],
+		ipBans: [],
+	};
+
+	const sessions = await db.findMany('sessions', {}, Infinity);
+	for (const session of sessions) {
+		dbBackup.sessions[session.sid] = {
+			username: session.username,
+			sessionIDHash: session.sidHash,
+			connected: false,
+		};
+	}
+
+	const messages = await db.findMany('messages', {}, Infinity, 0, { ts: 1 });
+	for (const message of messages) {
+		dbBackup.messages.push({
+			messageID: message.id,
+			sessionIDHash: message.sidHash,
+			message: message.message,
+			ts: message.ts,
+		});
+	}
+
+	const ipBans = await db.findMany('ipBans', {}, Infinity);
+	for (const ipBan of ipBans) {
+		dbBackup.ipBans.push(ipBan.ip);
+	}
+
+	writeFileSync('./data/backup.json', JSON.stringify(dbBackup, null, 2));
+
+	process.exit();
+};
+
+main();
