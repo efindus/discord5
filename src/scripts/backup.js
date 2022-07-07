36ec8950651589f94673cdb91066fb61ab16ac27
efindus <48939809+efindus@users.noreply.github.com>
2022-07-07 16:09:28 +0200

    adjust relevant scripts to new DB schema
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- src/scripts/backup.js
+++ src/scripts/backup.js
@@ -5,28 +5,19 @@ const main = async () => {
 	await db.connect();
 
 	const dbBackup = {
-		sessions: {},
+		users: {},
 		messages: [],
 		ipBans: [],
 	};
 
-	const sessions = await db.findMany('sessions');
-	for (const session of sessions) {
-		dbBackup.sessions[session.sid] = {
-			username: session.username,
-			sessionIDHash: session.sidHash,
-			connected: false,
-		};
+	const users = await db.findMany('users');
+	for (const user of users) {
+		dbBackup.users[user.uid] = user;
 	}
 
 	const messages = await db.findMany('messages', {}, { ts: 1 });
 	for (const message of messages) {
-		dbBackup.messages.push({
-			messageID: message.id,
-			sessionIDHash: message.sidHash,
-			message: message.message,
-			ts: message.ts,
-		});
+		dbBackup.messages.push(message);
 	}
 
 	const ipBans = await db.findMany('ipBans');
--- src/scripts/seed.js
+++ src/scripts/seed.js
@@ -2,22 +2,20 @@ const db = require('../utils/database');
 
 const main = async () => {
 	await db.connect();
-	await db.insertMany('sessions', [
+	await db.insertMany('users', [
 		{
-			sid: 'efindusFTW',
-			username: 'efindus',
-			sidHash: '1b103e73ac4bb641681419030d92ed3af163fa35f10b9c57f9aefce8257b7378',
-		},
-		{
-			sid: '[Server]',
+			uid: '691657194299387Server',
 			username: '[Server]',
-			sidHash: '0170fdf90e06a44d9fdb151332dd0c9e121854eca9280251a2f884d167820f92',
+			nickname: '[Server]',
+			password: '',
+			salt: '',
+			jwtSecret: '',
 		},
 	]);
 
 	await db.insertOne('messages', {
 		id: '1625650625672-randombull$hitfromcrypto',
-		sidHash: '1b103e73ac4bb641681419030d92ed3af163fa35f10b9c57f9aefce8257b7378',
+		uid: '691657194299387Server',
 		message: 'It\'s awesome!',
 		ts: 1625650625672,
 	});
