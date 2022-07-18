acf80939e32bb969b1ef179a7e8140da33a2a1ca
efindus <48939809+efindus@users.noreply.github.com>
2022-07-18 15:18:30 +0200

    Create ratelimit class
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

---
+++ src/utils/ratelimit.js
@@ -0,0 +1,62 @@
+class Ratelimit {
+	#limit;
+	#duration;
+	#startOffset;
+
+	#points = {};
+
+	/**
+	 * Create a ratelimit instance
+	 * @param {number} limit - Maximum allowed number of points
+	 * @param {number} duration - Time in miliseconds between resets
+	 */
+	constructor(limit, duration) {
+		this.#limit = limit;
+		this.#duration = duration;
+
+		setInterval(this.reset, this.#duration);
+		this.#startOffset = Date.now() % this.#duration;
+	}
+
+	/**
+	 * Consume a number of points from a key if possible
+	 * @param {string} key - The key to take the points from
+	 * @param {number} points - The number of points to take
+	 * @returns {boolean} Whether the request was within the limit or not
+	 */
+	consume(key, points = 1) {
+		if (!this.#points[key]) this.#points[key] = 0;
+		this.#points[key] += points;
+
+		if (this.#points[key] <= this.#limit) return true;
+		else return false;
+	}
+
+	/**
+	 * Reset accumulated points
+	 */
+	reset() {
+		this.#points = {};
+	}
+
+	/**
+	 * Point limit
+	 */
+	get limit() {
+		return this.#limit;
+	}
+
+	/**
+	 * Duration in ms between resets
+	 */
+	get duration() {
+		return this.#duration;
+	}
+
+	/**
+	 * The time in ms until next reset
+	 */
+	get timeUntilReset() {
+		return (this.#duration - (Date.now() % this.#duration) + this.#startOffset) % this.#duration;
+	}
+}
