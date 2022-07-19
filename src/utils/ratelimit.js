bb9262de6b3c3c2942ad56872c2fe733b714c9a0
efindus <48939809+efindus@users.noreply.github.com>
2022-07-19 17:17:57 +0200

    Create RatelimitManager class
    Signed-off-by: efindus <48939809+efindus@users.noreply.github.com>

--- src/utils/ratelimit.js
+++ src/utils/ratelimit.js
@@ -60,3 +60,53 @@ class Ratelimit {
 		return (this.#duration - (Date.now() % this.#duration) + this.#startOffset) % this.#duration;
 	}
 }
+
+class RatelimitManager {
+	/**
+	 * @type {Record<string, Ratelimit>}
+	 */
+	#ratelimits = {};
+
+	/**
+	 * Create a ratelimit instance with a given id
+	 * @param {string} id - Id of the ratelimit instance to create
+	 * @param {number} limit - Maximum allowed number of points
+	 * @param {number} duration - Time in miliseconds between resets
+	 */
+	create(id, limit, duration) {
+		if (this.#ratelimits[id]) throw new Error(`[Ratelimit Manager] ID already in use: ${id}`);
+
+		this.#ratelimits[id] = new Ratelimit(limit, duration);
+	}
+
+	/**
+	 * Consume a number of points from a key in a ratelimit instance if possible
+	 * @param {string} id - Id of the ratelimit instance to consume from
+	 * @param {string} key - The key to take the points from
+	 * @param {number} points - The number of points to take
+	 * @returns {boolean} Whether the request was within the limit or not
+	 */
+	consume(id, key, points) {
+		if (!this.#ratelimits[id]) throw new Error(`[Ratelimit Manager] Unknown ID: ${id}`);
+
+		return this.#ratelimits[id].consume(key, points);
+	}
+
+	/**
+	 * Reset accumulated points in an instance
+	 * @param {string} id - Id of the ratelimit instance to reset
+	 */
+	reset(id) {
+		if (!this.#ratelimits[id]) throw new Error(`[Ratelimit Manager] Unknown ID: ${id}`);
+
+		this.#ratelimits[id].reset();
+	}
+
+	/**
+	 * @param {string} id - Id of the ratelimit instance to get
+	 * @returns {Ratelimit}
+	 */
+	getInstance(id) {
+		return this.#ratelimits[id];
+	}
+}
